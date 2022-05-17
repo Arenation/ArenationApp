aa8ec28b6997400c9874b1832a2df565d4ccfbcb
Jose Ayazo <josemanuelayazo@gmail.com>
2022-05-16 23:57:15 -0500

    Terminado el listview con scroll horizontal para las reviews, y solucionado el bug del back button en el arena page

--- lib/pages/arena.dart
+++ lib/pages/arena.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:arenation_app/utils/functions/get_avg_score.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +13,7 @@ import 'package:arenation_app/utils/text_theme.dart';
 import '../services/httpstate.dart';
 import '../models/arenas/modelArena.dart';
 import 'package:arenation_app/widgets/image_carousel.dart';
+import 'package:intl/intl.dart';
 
 class Arena extends StatelessWidget {
   const Arena({Key? key}) : super(key: key);
@@ -25,24 +28,17 @@ class Arena extends StatelessWidget {
         elevation: 0,
         titleSpacing: 0,
         title: const Text("Arena"),
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-          child: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: Icon(
-              Icons.chevron_left_outlined,
-              color: CustomColors.secondaryDark,
-            ),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            Icons.chevron_left_outlined,
+            color: CustomColors.secondaryDark,
           ),
         ),
       ),
       body: Consumer<GetArenas>(
         builder: (_context, getArenas, child) {
-          return Column(children: [
-            Expanded(
-              child: _bodyPage(context, getArenas),
-            )
-          ]);
+          return _bodyPage(context, getArenas);
         },
       ),
     );
@@ -50,77 +46,117 @@ class Arena extends StatelessWidget {
 
   Widget _bodyPage(BuildContext bodyContext, GetArenas arenas) {
     return Padding(
-        padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-        child: Column(
-          children: [
-            FutureBuilder<Response>(
-              future: arenas.state == StateHttp.loading &&
-                      arenas.state != StateHttp.success
-                  ? arenas.getArena()
-                  : null,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return selectedArenaSkeleton(bodyContext);
-                } else if (snapshot.hasData) {
-                  var data = snapshot.data!.getData as DataArena;
-                  return arenaDetails(bodyContext, data.data);
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                } else {
-                  return arenaEmptyResult(bodyContext);
-                }
-              },
-            )
-          ],
-        ));
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(children: [
+        FutureBuilder<Response>(
+          future: arenas.state == StateHttp.loading &&
+                  arenas.state != StateHttp.success
+              ? arenas.getArena()
+              : null,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return selectedArenaSkeleton(bodyContext);
+            } else if (snapshot.hasData) {
+              var data = snapshot.data!.getData as DataArena;
+              return arenaDetails(bodyContext, data.data);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else {
+              return arenaEmptyResult(bodyContext);
+            }
+          },
+        ),
+      ]),
+    );
   }
 
   Widget arenaDetails(BuildContext context, Arenas arena) {
-    return Column(
-      children: [
-        arenaCarousel(context, arena),
-        arenaHeader(context, arena),
-        arenaDescription(context, arena),
-        arenaFacilities(context, arena),
-        // arenaReviews(context, arena.reviews),
-      ],
+    return Expanded(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          arenaCarousel(context, arena),
+          arenaHeader(context, arena),
+          arenaDescription(context, arena),
+          arenaFacilities(context, arena),
+          arenaReviews(context, arena.reviews),
+        ],
+      ),
     );
   }
 
   Widget arenaReviews(BuildContext context, List<Review> reviews) {
-    return const Text("Reviews");
-    // Padding(
-    //   padding: const EdgeInsets.only(top: 16.0),
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     children: [
-    //       Text(
-    //         "Reseñas",
-    //         style: CustomTextTheme.h2(context),
-    //       ),
-    //       Expanded(
-    //           child: SingleChildScrollView(
-    //         scrollDirection: Axis.horizontal,
-    //         child: Row(
-    //           children: [
-    //             for(var review in reviews){
-    //               return Container(
-    //                 width: 200,
-    //                 heigth: 200,
-    //               );
-    //             }
-    //           ],
-    //         ),
-    //       ))
-    //     ],
-    //   ),
-    // );
+    // return const Text("Reviews");
+    // return
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Reseñas",
+            style: CustomTextTheme.h2(context),
+          ),
+          Container(
+            width: double.infinity,
+            height: 120,
+            child: ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: [
+                for (var review in reviews) arenaReviewCard(context, review),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget arenaReviewCard(BuildContext context, Review review) {
+    return Container(
+      margin: const EdgeInsets.only(right: 16),
+      decoration: BoxDecoration(
+          color: CustomColors.secondaryLight,
+          borderRadius: BorderRadius.circular(8)),
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "User Name",
+                    style: CustomTextTheme.p200(
+                        context, CustomColors.secondaryDark,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(DateFormat.yMMMMd("en_US").format(review.date)),
+                ],
+              ),
+            ),
+            Text(
+              review.content,
+              // overflow: TextOverflow.ellipsis,
+              textWidthBasis: TextWidthBasis.parent,
+              style: CustomTextTheme.p100(context, CustomColors.secondaryDark),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget arenaFacilities(BuildContext context, Arenas arena) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.symmetric(vertical: 16.0),
+      margin: const EdgeInsets.only(top: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
